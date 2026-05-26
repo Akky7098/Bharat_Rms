5aca03462a161a0cafec98d6dac023be057700ab
Akky7098 <ankitsinvns@gmail.com>
2026-05-26 14:50:26 +0530

    sales insight

--- backend/cron/salesDailyInsightCron.js
+++ backend/cron/salesDailyInsightCron.js
@@ -32,7 +32,7 @@ const startSalesDailyInsightCron = () => {
   );
 
   cron.schedule(
-    "50 14 * * *",
+    "55 14 * * *",
     async () => {
       try {
         console.log("Sales daily insight cron started at 8:00 PM IST");
